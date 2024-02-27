fe34749ef95545c5c1f296c43f938b92f2109d32
ColotVS <kolotilov98@mail.ru>
2024-02-27 14:59:56 +0700

    Работа с содержимым файла Урок 16, задача 39

---
+++ lesson_16_39.java
@@ -0,0 +1,10 @@
+public class lesson_16_39 {
+    //Работа с содержимым файла
+    //И наконец, есть целая серия методов, которые позволяют легко прочитать или записать содержимое файла. Пример:
+
+    //Код	                                                Описание
+    //Path path = Path.of("c:\\readme.txt");
+    //List<String> list = Files.readAllLines(path);         Читаем содержимое файла в виде списка строк.
+    //for (String str : list)
+    //   System.out.println(str);                           Выводим строки на экран
+}
