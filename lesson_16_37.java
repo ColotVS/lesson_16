e5c00b7cd769e0e74c9be5c8d5d36dc167487e1a
ColotVS <kolotilov98@mail.ru>
2024-02-27 14:55:29 +0700

    Копирование, перемещение и удаление Урок 16, задача 37

---
+++ lesson_16_37.java
@@ -0,0 +1,19 @@
+public class lesson_16_37 {
+    //Копирование, перемещение и удаление
+
+    //Копировать, перемещать и удалять файлы так же легко. На директории это тоже распространяется, но они должны быть пустые.
+
+    //Код	                                            Примечание
+    //Path path1 = Path.of("c:\\readme.txt");
+    //Path path2 = Path.of("c:\\readme-copy.txt");
+    //Files.copy(path1, path2);                         Копирует файл
+
+
+    //Path path1 = Path.of("c:\\readme.txt");
+    //Path path2 = Path.of("d:\\readme-new.txt");
+    //Files.move(path1, path2);                         Перемещает и переименовывает файл
+
+
+    //Path path = Path.of("d:\\readme-new.txt");
+    //Files.delete(path);                               Удаляет файл
+}
